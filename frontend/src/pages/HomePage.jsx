48cdcf76b00d9b19a516b5cf9746b9d2bd3072e0
mjdevs27 <moksh1027@gmail.com>
2025-09-11 22:04:11 +0530

    UI built

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,24 @@
-import React from "react"
+import { useChatStore } from "../store/useChatStore";
 
-const HomePage = ()=>{
-    return(
-        <div>
-            Hey I m HomePage
+import Sidebar from "../components/Sidebar";
+import NoChatSelected from "../components/NoChatSelected";
+import ChatContainer from "../components/ChatContainer";
+
+const HomePage = () => {
+  const { selectedUser } = useChatStore();
+
+  return (
+    <div className="h-screen bg-base-200">
+      <div className="flex items-center justify-center pt-20 px-4">
+        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
+          <div className="flex h-full rounded-lg overflow-hidden">
+            <Sidebar />
+
+            {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+          </div>
         </div>
-    ) 
-
-
-}
-
-export default HomePage
+      </div>
+    </div>
+  );
+};
+export default HomePage;
