80d72b0b1177736d0009008e7df10a268be78d75
mjdevs27 <moksh1027@gmail.com>
2025-09-11 10:48:08 +0530

    routes updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,13 @@ import{ Routes , Route }from "react-router-dom"
 import Navbar from "./components/Navbar.jsx"
 import HomePage from "./pages/HomePage.jsx"
 import SignUpPage from "./pages/SignUpPage.jsx"
+import LoginPage from "./pages/LoginPage.jsx"
+import ProfilePage from "./pages/Profile.jsx"
 import { useAuthStore } from "./store/useAuthStore.js"
 import { Toaster } from "react-hot-toast"
 import { Loader } from "lucide-react"
 import { Navigate } from "react-router-dom"
-import LoginPage from "./pages/LoginPage.jsx"
+
 const App = ()=>{
   const {checkAuth , authUser, isCheckingAuth} = useAuthStore()
 
@@ -37,7 +39,7 @@ const App = ()=>{
         <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
         <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
         {/* <Route path="/settings" element={<SettingsPage />} /> */}
-        {/* <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} /> */}
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
 
 
