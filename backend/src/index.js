fa8703fe22f73b71cf57e52229abed5f77966da8
mjdevs27 <moksh1027@gmail.com>
2025-10-28 05:14:07 +0530

    Deployment begins

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,9 @@ import cookieParser from "cookie-parser"
 import { connectDB } from "./utils/db.utils.js";
 import cors from "cors"
 import { app , server } from "./utils/socket.js";
+
+import path from "path";
+
 dotenv.config()
 
 
@@ -13,8 +16,7 @@ dotenv.config()
 // const app = express();
 
 const port = process.env.PORT
-
-
+const __dirname = path.resolve()
 
 app.use(express.json({limit:"5mb"}));
 app.use(express.urlencoded({ limit: "10mb", extended: true })); 
@@ -28,6 +30,14 @@ app.use(cors({
 app.use("/api/auth" , authRoutes)
 
 app.use("/api/messages" , messageRoutes)
+
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname , "../frontend/dist")));
+
+    app.get("*" ,(req,res)=>{
+        res.sendFile(path.join(__dirname , "../frontend" , "dist" , "index.html"));
+    });
+}
 
 server.listen(port , ()=>{
     console.log("server is running", port)
